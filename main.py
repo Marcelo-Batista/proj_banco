7fa88dfa187cc8eb83b11cc4eb92935c2e480cd5
Marcelo Batista <marcelo.silva238@fatec.sp.gov.br>
2025-02-21 01:11:44 -0300

    edit: modularizacao das funcoes em metodos depositar, sacar e deposito

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from datetime import datetime
+
+def depositar():
+    valor = float(input("Digite o valor a ser depositado: "))
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:           R$ {valor:,.2f}\n"
+    else:
+        print("Valor inválido.")
+
+def sacar():
+    if quantidade_saques < LIMITE_QUANT_SAQUES:
+        valor = float(input("Digite o valor a ser sacado: "))
+        if valor > 0 and valor <= LIMITE_SAQUE and valor <= saldo:
+            saldo -= valor
+            extrato += f"Saque:              R$ {valor:,.2f}\n"
+            quantidade_saques += 1
+        else:
+            print("Valor inválido.")
+    else:
+        print("Limite de saques excedido.")
+
+def extrato():
+    print("Extrato:".center(44, "-"))
+    print("Não foram realizadas operações até o momento" if not extrato else extrato)
+    print(f"Saldo:              R$ {saldo:,.2f}")
+
 menu = '''
 
 [d] Depositar
@@ -6,6 +33,7 @@ menu = '''
 [q] Sair
 
 '''
+data_hoje = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
 saldo = 0
 extrato = ""
 quantidade_saques = 0
@@ -15,27 +43,11 @@ LIMITE_QUANT_SAQUES = 3
 while True:
     opção = input(menu)
     if opção == 'd':
-        valor = float(input("Digite o valor a ser depositado: "))
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito:           R$ {valor:,.2f}\n"
-        else:
-            print("Valor inválido.")  
+        depositar()  
     elif opção == 'w':
-        if quantidade_saques < LIMITE_QUANT_SAQUES:
-            valor = float(input("Digite o valor a ser sacado: "))
-            if valor > 0 and valor <= LIMITE_SAQUE and valor <= saldo:
-                saldo -= valor
-                extrato += f"Saque:              R$ {valor:,.2f}\n"
-                quantidade_saques += 1
-            else:
-                print("Valor inválido.")
-        else:
-            print("Limite de saques excedido.")
+        sacar()
     elif opção == 's': 
-        print("Extrato:".center(44, "-"))
-        print("Não foram realizadas operações até o momento" if not extrato else extrato)
-        print(f"Saldo:              R$ {saldo:,.2f}")
+        extrato()
     elif opção == 'q':
         break
     else:
