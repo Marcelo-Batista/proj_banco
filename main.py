6f67e5539e8a73e2cbdd2793ad9674ad6c7335da
Marcelo Batista <marcelo.silva238@fatec.sp.gov.br>
2025-02-24 21:02:11 -0300

    edit: criacao dos metodos get_data, get_hora e set_extrato e adaptacao de codigo para recebe-los

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-from datetime import datetime
+from datetime import date, time
+
+def get_data():
+    return date.now().strftime("%d/%m/%Y")
+
+
+def get_hora():
+    return time.now().strftime("%H:%M:%S")
 
 def depositar():
     valor = float(input("Digite o valor a ser depositado: "))
     if valor > 0:
         saldo += valor
-        extrato += f"Depósito:           R$ {valor:,.2f}\n"
+        set_extrato("deposito", valor)
+        #extrato += f"Depósito:           R$ {valor:,.2f}\n"
     else:
         print("Valor inválido.")
 
 def sacar():
-    if quantidade_saques < LIMITE_QUANT_SAQUES:
+    if not extrato["extrato"].get(get_data) or extrato["extrato"][get_data].count() <= LIMITE_QUANT_SAQUES:
         valor = float(input("Digite o valor a ser sacado: "))
         if valor > 0 and valor <= LIMITE_SAQUE and valor <= saldo:
             saldo -= valor
-            extrato += f"Saque:              R$ {valor:,.2f}\n"
-            quantidade_saques += 1
+            set_extrato("saque", valor)
+            #extrato += f"Saque:             -R$ {valor:,.2f}\n"
+            #quantidade_saques += 1
         else:
             print("Valor inválido.")
     else:
         print("Limite de saques excedido.")
 
-def extrato():
+def set_extrato(tipo_trasacao, valor):
+    if extrato["extrato"].get(get_data()):
+        extrato["extrato"][get_data()].append({get_time():{"tipo_transacao": tipo_trasacao, "valor": valor}})
+    else:
+        extrato["extrato"].append({get_data: {get_time():{"tipo_transacao": tipo_trasacao, "valor": valor}}})
+    
+
+def get_extrato():
     print("Extrato:".center(44, "-"))
     print("Não foram realizadas operações até o momento" if not extrato else extrato)
     print(f"Saldo:              R$ {saldo:,.2f}")
@@ -33,12 +49,12 @@ menu = '''
 [q] Sair
 
 '''
-data_hoje = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+
 saldo = 0
-extrato = ""
+extrato = dict.fromkeys("extrato", "")
 quantidade_saques = 0
 LIMITE_SAQUE = 500
-LIMITE_QUANT_SAQUES = 3
+LIMITE_QUANT_SAQUES = 10
 
 while True:
     opção = input(menu)
